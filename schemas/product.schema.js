98c042d50dd8a78260cc10144426e1ad3b5d8bb2
Manuela Vargas Herrera <manuelavargasherrera@gmail.com>
2025-03-10 21:20:51 -0500

    v3 migraciones productos y categorias

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -3,22 +3,27 @@ const Joi = require('joi');
 const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
-const description = Joi.string.min(10);
+const description = Joi.string().min(10);
+const categoryId = Joi.number().integer();
+
 
 const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
-  description:description.require(),
-  image: image.required()
+  description:description.required(),
+  image: image.required(),
+  categoryId: categoryId.required(),
+
 });
 
 const updateProductSchema = Joi.object({
   name: name,
   price: price,
   description:description,
-  image: image
+  image: image,
+  categoryId
 });
 
 const getProductSchema = Joi.object({
